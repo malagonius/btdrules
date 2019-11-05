5c0c748ac1789d1f68015e329107ea22501971a9
malagonius <47065208+malagonius@users.noreply.github.com>
2019-11-05 14:59:28 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,149 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="btd.css">
+</head>
+<body>
+	<div class="title_container">
+		<h1>BTD Board Game</h1>
+		<div class="title_image"></div>
+	</div>
+	<div class="content_container">
+		<div class="navigation">
+			<div class="navigation_container">
+				<a class="active" id ="1" href="#"><span>Rules</span></a>
+				<a id ="2" href="#"><span>Content</span></a>	
+				<a id ="3" href="#"><span>Turn</span></a>	
+				<a id ="4" href="#"><span>Contacts</span></a>		
+			</div>
+		</div>
+			<div id="content1" class="content active">
+				<div>
+					<span>
+						<ul><li>How to win</li>
+								<ul><li>Win the Golden Banana</li>
+								<ul>
+									<li>Build the Monkey Temple Out of the money support tower, once it finishes sacrifice the golden monkey to earn the golden banana</li>
+								</ul>
+								<li>Be the only surviving money leader</li>
+								<ul>
+									<li>Build the Monkey Temple Out of the money support tower, once it finishes sacrifice the golden monkey to earn the golden banana
+									</li>
+								</ul>
+								<li>Earn the most respect from monkey</li>
+									<ul>
+										<li> Every round there will be bloons trying to attack the village, your purpose is to defend the village with your monkeys... Whenever a bloon passes by, you love a life.
+										At 0 life you are out.</li>
+										<li> There will also be Moab Class baloons, if those passes by you will be out in any case.
+										</li>
+										<li>Certain monkey tower can increase your current lives</li>
+									</ul>
+								</li>
+							</ul>
+						</ul>
+					</span>
+				</div>		
+			</div>
+			<div id="content2" class="content">	
+				<div>
+					<span>
+						<ul>
+							<li>4 Type of Monkeys</li>
+							<ul>
+								<li>
+									Standard --> Very High Quality/Price value Strong most cases, low efficency, can't pop lead
+									<ul>
+										<li>Dart<ul>
+											<li><a id="dart1">Sharp Shooting > Spiked Balls > Ultra Juggernaut</a></li>
+											<li><a id="dart2"></a>Incrase Range > Extra firing > Crossbow Master</li>
+										</ul></li>
+										<li>Boomerang<ul>
+											<li><a id="rang1"></a>SharpRang > FlameRang > LaserRang</li>
+											<li><a id="rang2"></a> ExtraRange > Epic Range > Permacharge</li>
+										</ul></li>
+										<li>TackShooter<ul>
+											<li><a id="tack1"></a>More Tacks > Even More Tacks > Tack-nator</li>
+											<li><a id="tack2"></a>Hard tacks > Rusty Tacks > Huge Tacks</li>
+										</ul></li>
+									</ul>
+								<li>Military --> Can pop Lead, Good for Crowd Controll, can be placed almost everywhere
+									<ul>
+										<li>Sniper<ul>
+											<li><a id="snip1"></a>Increased Accuracy > Deadly Precision > Full Metal Jacket</li>
+											<li><a id="snip2"></a>Faster Firing  > Semi Automatic Rifle > Elite Defender</li>
+										</ul></li>
+										<li>Submarine<ul>
+											<li><a id="subb1"></a>Rocket Launcher > First Strike > Nuclear Missle</li>
+											<li><a id="subb2"></a> Submerge > Radioactive Submarine > Atomic radiation</li>
+										</ul></li>
+										<li>HeilPilot<ul>
+											<li><a id="pilo1"></a>Faster firing > Razor Rotors > Apache Prime</li>
+											<li><a id="pilo2"></a>Ultra Jet > Chase > Comache Defence</li>
+										</ul></li>
+									</ul></li>
+								<li>Magical --> Very expensive, High efficency,Can't pop certain bloons
+									<ul>
+										<li>Wizzard<ul>
+											<li><a id="wizz1"></a>Intese Magic > Arcane Spikes > Archmage</li>
+											<li><a id="wizz2"></a>Fire Ball > Wall Of Fire > PoenixLord</li>
+										</ul></li>
+										<li>Druid<ul>
+											<li><a id="drud1"></a>Chain Lighting > Lighting Ball > StormLord</li>
+											<li><a id="drud2"></a>Hard Throns > Grasping Thorns > Spirit Of The Forest</li>
+										</ul></li>
+										<li>SuperMonkey<ul>
+											<li><a id="supm1"></a>Laser Vision > Plasma Ray > Sun God</li>
+											<li><a id="supm2"></a>Epic Range > Robo Monkey > BloonsDay</li>
+										</ul></li>
+									</ul></li>
+								<li>Support --> can't pop bloons, give cool and unique effects
+									<ul>
+										<li>Banana Farm<ul>
+											<li><a id="farm1"></a>Increase Production > Banana Republic > Bananopoli</li>
+											<li><a id="farm2"></a>Valuable Banana > Banananomics > BnanaBank</li>
+										</ul></li>
+										<li>Monkey Village<ul>
+											<li><a id="vill1"></a>Jungle Drums > Camo Detections > Inspiring Citadel</li>
+											<li><a id="vill2"></a>Banana Accolyte > Banana Temple > Golden Banana</li>
+										</ul></li>
+									</ul></li>
+							</ul>
+						</ul>
+					</span>
+				</div>			
+			</div>	
+			<div id="content3" class="content">		
+				<div>
+					<span>
+						Honestly, what did you expect?<br><br>
+						-Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+						-Aenean in metus dictum, laoreet magna vitae, cursus ante.<br>
+						-Vestibulum sagittis metus sit amet tincidunt pulvinar.<br>
+						-Fusce semper lacus sed tristique dignissim.<br>
+						-Integer vel metus ultrices, euismod orci eu, tempus odio.<br>
+						-Curabitur porttitor diam nec rutrum maximus.<br>
+						-Proin eu mi faucibus, varius mi a, fermentum felis.<br>
+						-Ut mollis magna ac varius efficitur.<br>
+					</span>
+				</div>
+			</div>	
+			<div id="content4" class="content">
+				<div>
+					<span>
+						Telephone : 1234567890<br>
+						Email : t.o@fregato.ora<br>
+						Fax : lol<br>
+						Address : Viale Manidal, Naso 1337
+					</span>
+				</div>		
+			</div>		
+		</div>
+	</div>
+	<div class="modal">
+		<div class="modal_container">
+			<div class="modal_title"></div>
+			<div class="modal_content"></div>
+		</div>
+	</div>
+</body>
+<script type="text/javascript" src="btd.js"></script>
+</html>
